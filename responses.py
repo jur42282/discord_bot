96da8da843d42402eb5181a798df56aaf05b6a1a
jur42282 <jur42282@spseol.cz>
2023-11-15 18:36:03 +0100

    Basic functions

---
+++ responses.py
@@ -0,0 +1,22 @@
+import random as rd
+
+greetings = ["Hey!", "Hi there!", "Howdy", "Hello"]
+colors = ["Red", "Black", "Green"]
+
+
+
+def get_response(message: str) -> str:
+    p_message = message.lower()
+
+    if p_message == "!hello":
+        return rd.choice(greetings)
+    
+
+    if p_message == "!gamba":
+        results = rd.choices(colors, weights = [18, 18, 2], k = 1)
+        return str(results)
+    
+
+    if p_message == "!help":
+        return "`There's no help`"
+     
